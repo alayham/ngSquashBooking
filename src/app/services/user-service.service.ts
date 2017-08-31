b28c5f6fcaf6ef25c95b822f547c877ea583af78
Alayham Saleh <alayham@hotmail.com>
2017-08-31 10:30:59 +0200

    The User Service basic API

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -1,8 +1,20 @@
 import { Injectable } from '@angular/core';
+import { IUser } from "app/services/IUser";
 
 @Injectable()
 export class UserServiceService {
 
   constructor() { }
 
+  getUserList(): IUser[]{
+    return [];
+  }
+
+  getUser(uid:number):IUser{
+    return;
+  }
+
+  logInUser(userName:string, userPassword: string): boolean{
+    return false;
+  }
 }
