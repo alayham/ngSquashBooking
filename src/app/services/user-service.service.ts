9bfd45d59494fafd5cf30f81df1a81dbbf82e936
Alayham Saleh <alayham@hotmail.com>
2017-08-31 11:10:09 +0200

    Generating up to 10 users

--- src/app/services/user-service.service.ts
+++ src/app/services/user-service.service.ts
@@ -3,15 +3,24 @@ import { IUser } from "app/services/IUser";
 
 @Injectable()
 export class UserService {
-
-  constructor() { }
+  _totalUsers: number;
+  _userList:IUser[] = [{userName: 'admin', userPassword: 'pass'}];
+  constructor() {
+    this._totalUsers = Math.floor((Math.random() * 10 + 1));
+    for(let i=1; i<=this._totalUsers; i++){
+      this._userList[i] = {
+        userName: 'user' + i,
+        userPassword: 'pass' + i,
+      }
+    }
+  }
 
   getUserList(): IUser[]{
-    return [];
+    return this._userList;
   }
 
   getUser(uid:number):IUser{
-    return;
+    return this._userList[uid];
   }
 
   logInUser(userName:string, userPassword: string): boolean{
