55d1e6985375ae7e493a69a46b89a2cdd6bcff3b
Alayham Saleh <alayham@hotmail.com>
2017-09-15 11:11:55 +0200

    Some optimizations for small displays.

--- src/app/ui/court-calendar/court-calendar.component.ts
+++ src/app/ui/court-calendar/court-calendar.component.ts
@@ -17,10 +17,25 @@ import { ReservationsService } from "app/services/reservations.service";
 })
 export class CourtCalendarComponent {
 
+  now: Date = new Date();  
+  _dateInput: number = this.now.valueOf();
+  scheduleDay:Date = this.now;
+  forwardDays:Date[] = this.schedulerService.getForwardDays(this.now);
   @Input() court:ICourt;
 
+  set dateInput(value: number){
+    this._dateInput = +value;
+    this.setDate( +value);
+  }
+
+  get dateInput():number{
+    return this._dateInput;
+  }
   constructor(public schedulerService: SchedulerService, public reservationService:ReservationsService) { 
 
   }
 
+  setDate(dateValue:number){
+    this.scheduleDay = new Date(dateValue);
+  }  
 }
